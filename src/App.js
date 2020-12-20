50de0c8d00343966d64fe37bbee3ce53c0c1462b
Rio Kawasaki <r.kwsk.0612@gmail.com>
2020-12-20 19:28:44 +0900

    refactering object-and-useState branch

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import React, { useState } from 'react';
 const App = props => {
 
   const [state, setState] = useState(props)
-  // const [name, setName] = useState(props.name)
-  // const [price, setPrice] = useState(props.price)
-
-  const reset = () =>{
-    // setPrice(props.price)
-    // setName(props.name)
-    setState(props)
-  }
+  const { name, price} = state//分割代入
 
   return (
-    // 空タグは<React.Fragment>と同じ
     <>
-      <p>現在の{state.name}は，{state.price}円です</p>
-      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
-      {/* props.price自体は普遍．変化するのはuseStateで渡した変数のみ */}
-      <button onClick={reset}>Reset</button>
-      <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
+      <p>現在の{name}は，{price}円です</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   );
 }
