cddc933bc25619d7dc66867c486c203edc1949e5
Rio Kawasaki <r.kwsk.0612@gmail.com>
2020-12-20 19:24:00 +0900

    implement object and useState

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 
 const App = props => {
 
-  // useStateを使用することにより状態を持つことができる．
-  // 従って，以下の文がないとjsx内で変数を持つことができない．
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+  const [state, setState] = useState(props)
+  // const [name, setName] = useState(props.name)
+  // const [price, setPrice] = useState(props.price)
 
   const reset = () =>{
-    setPrice(props.price)
-    setName(props.name)
+    // setPrice(props.price)
+    // setName(props.name)
+    setState(props)
   }
 
   return (
     // 空タグは<React.Fragment>と同じ
     <>
-      <p>現在の{name}は，{price}円です</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <p>現在の{state.name}は，{state.price}円です</p>
+      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
       {/* props.price自体は普遍．変化するのはuseStateで渡した変数のみ */}
       <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   );
 }
