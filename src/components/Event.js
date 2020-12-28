75495e97c6622648d5ad67b96259fa0d00c423f0
Rio Kawasaki <r.kwsk.0612@gmail.com>
2020-12-28 10:55:15 +0900

    implement confirming function for delete button

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,17 +4,20 @@ import React from 'react';
 const Event = ({ dispatch, event }) => {
     const id = event.id
      const handleClickDeleteButton = () => {
-        dispatch({ 
+        const result = window.confirm(`id${id}のイベントを本当に削除しますか？`)
+        if (result) {
+            dispatch({ 
             type: 'DELETE_EVENT',
             id,//id: idの短縮系
-        })
+            })
         }
+    }
     return (
         <tr>
-        <td>{event.id}</td>
-        <td>{event.title}</td>
-        <td>{event.body}</td>
-        <td><button type='button' className='btn btn-danger' onClick={handleClickDeleteButton}>削除</button></td>
+            <td>{event.id}</td>
+            <td>{event.title}</td>
+            <td>{event.body}</td>
+            <td><button type='button' className='btn btn-danger' onClick={handleClickDeleteButton}>削除</button></td>
         </tr>
     )
 }
